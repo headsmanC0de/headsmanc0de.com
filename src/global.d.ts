699da0f7c298460b936a6c74960fb6430d34b946
Назарій Петрик <headsmanc0de@MacBookPro.lan>
2026-02-20 13:58:25 +0200

    fix(ts): add global.d.ts for CSS module declarations

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+declare module "*.css" {
+	const content: Record<string, string>;
+	export default content;
+}
+
+declare module "*.scss" {
+	const content: Record<string, string>;
+	export default content;
+}
+
+declare module "*.sass" {
+	const content: Record<string, string>;
+	export default content;
+}
